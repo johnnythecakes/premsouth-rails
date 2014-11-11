cd16f6dc7a344bf36860695f20ccbaad55a2ba91
johnnythecakes <1zachjohnson@gmail.com>
2014-11-10 20:25:03 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PremSouthRails::Application.config.secret_key_base = 'e8beb3a7f9a53378ed57c49bfe1f314b3216bfd3a548332727a534ad474ede3028545d8174bcd5d82974fd251b05fc3861cc740977a52097037de0fe0fdbae3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PremSouthRails::Application.config.session_store :cookie_store, key: '_prem_south_rails_session'
